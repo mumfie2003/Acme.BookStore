08fc03fd33617928b105b893df015279ce6b90ae
Colin Munford <colin_munford@hotmail.com>
2020-05-12 19:21:11 +0100

    Try property injection - same problem

---
+++ src/Acme.BookStore.Application/Test/EmailSendingJob.cs
@@ -0,0 +1,49 @@
+﻿using Acme.BookStore.BookStore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Test
+{
+    public class EmailSendingJob : BackgroundJob<EmailSendingArgs>, ITransientDependency
+    {
+        public IRepository<Book, Guid> BookRepository { 
+            get { return _bookRepository; } 
+            set { 
+                _bookRepository = value; } }
+        private IRepository<Book, Guid> _bookRepository;
+        public EmailSendingJob()
+        {
+
+        }
+        //public EmailSendingJob(IRepository<Book, Guid> bookRepository)
+        //{
+        //   // _bookRepository = bookRepository;
+        //}
+
+        public override void Execute(EmailSendingArgs args)
+        {
+            const string MODULE = "Execute";
+            MyLogIt(MODULE, "BEGIN");
+            var query = BookRepository
+                .Where(p => p.Price > 10.0);
+            var results = query.ToList();
+
+            MyLogIt(MODULE, "END");
+
+        }
+
+        private static void MyLogIt(string module, string output)
+        {
+            using (StreamWriter sw = File.AppendText(@"E:\abd-Projects\TvGuide\Src\BackgroundJobs\aspnet-core\src\AppsByDesign.BackgroundJobs.Web\Logs\debugit.txt"))
+            {
+                sw.WriteLine($"{DateTime.Now.TimeOfDay} {module}->{output}");
+            }
+        }
+    }
+}
---
+++ src/Acme.BookStore.Application/TestAppService.cs
@@ -0,0 +1,50 @@
+﻿using Acme.BookStore.BookStore;
+using Acme.BookStore.Test;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore
+{
+    public class TestAppService : ApplicationService
+    {       
+        private readonly IBackgroundJobManager _backgroundJobManager;
+        private readonly IRepository<Book, Guid> _bookRepository; 
+
+        public TestAppService(IBackgroundJobManager backgroundJobManager,
+             IRepository<Book, Guid> bookRepository)
+        {
+            _backgroundJobManager = backgroundJobManager;
+            _bookRepository = bookRepository;
+        }
+
+        public async Task CreateBackgroundJob(string userName, string emailAddress, string password)
+        {
+            var args = new EmailSendingArgs
+            {
+                EmailAddress = emailAddress,
+                Subject = "You've successfully registered!",
+                Body = "..."
+            };
+
+            await _backgroundJobManager.EnqueueAsync(args);
+        }
+        public void ExecuteJobDirect(string userName, string emailAddress, string password)
+        {
+           
+            var args = new EmailSendingArgs
+            {
+                EmailAddress = emailAddress,
+                Subject = "You've successfully registered!",
+                Body = "..."
+            };
+            EmailSendingJob job = new EmailSendingJob();
+            job.BookRepository = _bookRepository;
+            job.Execute(args);
+        }
+    }
+}
